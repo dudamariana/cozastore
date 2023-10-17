93d34e3a759da99e88cdfc7254afd09e5ec291c7
dudamariana <maria.botura@etec.sp.gov.br>
2023-10-17 11:33:17 -0300

    Criação das Classes

---
+++ CozaStore/Models/Cor.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CozaStore.Models;
+
+[Table("Cor")]
+
+    public class Cor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public byte Id {get; set; }
+
+        [Required(ErrorMessage = "Informe o Nome")]
+        [StringLength(30, ErrorMessage = "O nome deve possuir no máximo 30 caracteres")]
+        public string Nome {get; set; }
+        
+           [Display(Name = "Código da Cor")]
+           [Required(ErrorMessage = "Informe o Código da cor")]
+        [StringLength(30, ErrorMessage = "O código da cor deve possuir no máximo 7 caracteres")]
+        public string CodigoHexa {get; set; }
+    }
---
+++ CozaStore/Models/Tamanho.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CozaStore.Models;
+
+[Table("Tamanho")]
+
+    public class Tamanho
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public byte Id {get; set; }
+
+          [Required(ErrorMessage = "Informe a sigla")]
+        [StringLength(30, ErrorMessage = "A sigla deve possuir no máximo 5 caracteres")]
+        public string CodigoHexa {get; set; }
+
+        [Required(ErrorMessage = "Informe o Nome")]
+        [StringLength(30, ErrorMessage = "O nome deve possuir no máximo 30 caracteres")]
+        public string Nome {get; set; }        
+    }
